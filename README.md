c005c8195725d9c4fe6dc6dd12f476d1b0a91af0
Carlos A <cayala.w@gmail.com>
2019-05-16 09:23:35 -0500

    Set base files for repo

---
+++ README.md
@@ -0,0 +1,15 @@
+Mini Assignments: Week 12 Day 04: Functions & Functional Programming
+
+Solve the following exercises:
+
+    https://www.codewars.com/kata/calculating-with-functions/javascript
+    https://www.codewars.com/kata/57ee4a67108d3fd9eb0000e7Link Text
+    https://www.codewars.com/kata/52195c9bb576caf14200007f
+    https://www.codewars.com/kata/5828713ed04efde70e000346
+    https://www.codewars.com/kata/56ba8a9b022c16017d0001f3
+
+Extra challenge:
+
+    https://www.codewars.com/kata/54ca3e777120b56cb6000710
+    https://www.codewars.com/kata/539a0e4d85e3425cb0000a88
+
---
+++ index.js
@@ -0,0 +1,34 @@
+// Mini Assignments: Week 12 Day 04
+// EX01
+// https://www.codewars.com/kata/calculating-with-functions/javascript
+
+
+
+// EX02
+// https://www.codewars.com/kata/57ee4a67108d3fd9eb0000e7Link Text
+
+
+
+// EX03
+// https://www.codewars.com/kata/52195c9bb576caf14200007f
+
+
+
+// EX04
+// https://www.codewars.com/kata/5828713ed04efde70e000346
+
+
+
+// EX05
+// https://www.codewars.com/kata/56ba8a9b022c16017d0001f3
+
+
+
+// EX06
+// https://www.codewars.com/kata/54ca3e777120b56cb6000710
+
+
+
+// EX07
+// https://www.codewars.com/kata/539a0e4d85e3425cb0000a88
+
